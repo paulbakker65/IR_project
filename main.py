50c85bbdd4c6bc84fa956906ef79a021275e3fc6
paulbakker65 <P.Bakker-2@student.tudelft.nl>
2018-04-10 17:11:10 +0200

    Updated final datasets
    Forgot to include original ambiguous query

--- main.py
+++ main.py
@@ -7,22 +7,20 @@ import wikipedia
 import sys
 import lda_modelling as lm
 
-
-
 if __name__ == '__main__':
 
-    queries = ['jaguar', 'hertz',  'shell', 'apple', 'fish', 'mouse', 'bear', 'duck']
+    queries = ['jaguar', 'hertz', 'shell', 'apple', 'fish', 'mouse', 'bear', 'duck']
 
     for query in queries:
 
         try:
             page = wikipedia.summary(query + ' disambiguation')
-            queries_temp = wikipedia.search(query, results=5)
+            queries_temp = np.append([query], wikipedia.search(query, results=10))
         except wikipedia.exceptions.DisambiguationError as e:
-            queries_temp = e.options[:5]
+            queries_temp = np.append([query], e.options[:10])
 
         # queries_temp = ga.autocomp(query)
-        result_scrape = sc.scrape(queries_temp, 'google', 5)
+        result_scrape = sc.scrape(queries_temp, 'google', 3)
 
         results_cluster = km.kmeans(result_scrape, query)
         # lm.lda_topicmodeling(result_scrape[2], query)
